176cfc5af0be4a1cd64d32b973dc20cb22bea169
Murtaza Zaidi <zaidimurtaza102@gmail.com>
2024-12-04 13:47:03 +0530

    Update Navbar.jsx

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
         {/* <img className='mx-2 w-10 ' src={mzicon} alt='logo'/> */}
     </div>
     <div className="m-8 flex items-center justify-center gap-4 text-2xl md:items-center">
-        <a href="https://www.linkedin.com/in/mohd-murtaza-57816b302/" target="_blank" rel="noopener noreferrer"> <FaLinkedin/></a>
+        <a href="https://www.linkedin.com/in/murtaza-57816b302/" target="_blank" rel="noopener noreferrer"> <FaLinkedin/></a>
         <a href="https://github.com/zaidimurtaza" target="_blank" rel="noopener noreferrer"><FaGithub/></a>
         <a href="https://x.com/ZaidiMurtaza1" target="_blank" rel="noopener noreferrer"><FaXTwitter/></a>
         <a href="https://www.instagram.com/xydi_murtaza/" target="_blank" rel="noopener noreferrer"><FaInstagram/></a>
